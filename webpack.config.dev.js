1854b7b642ed2701e31c2d56856c59ee62f669e5
Chris Pachomski <chris.pachomski@borngroup.com>
2015-11-19 14:13:09 -0500

    remove boilerplate and add package.json

---
+++ webpack.config.dev.js
@@ -0,0 +1,34 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    'webpack-hot-middleware/client',
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  resolve:{
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [{
+      test: /\.js|\.jsx$/,
+      loaders: ['babel'],
+      include: path.join(__dirname, 'src')
+    },
+    {
+      test: /\.less$/,
+      loader: "styles!css!less"
+    }
+    ]
+  }
+};
